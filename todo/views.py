28e4cbbd82dd4e1f80729509a5892d2f7a20ac55
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2023-03-30 16:33:31 +0900

    delete機能の追加

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,37 @@
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from todo.TodoForm import TodoForm
+from todo.models import Todo
+
 
 # Create your views here.
 
 
+# def index(request):
+#     # if request.method == 'POST':
+#     #     form = TodoForm(request.POST)
+#     #     form.save()
+#     # else:
+#     form = TodoForm()
+#     # todo_lists = Todo.objects.all()
+#     # context = {"todo_lists": todo_lists}
+#     return render(request, 'todo.html', {'form': form})
+
 def index(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        form.save()
+        return redirect('/')
+    form = TodoForm()
+    todo_lists = Todo.objects.all()
+    context = {
+        "todo_lists": todo_lists,
+        'form': form
+        }
+    return render(request, 'todo.html', context)
+
+
+def delete(request):
+    todo_id = request.POST['id']
+    Todo.objects.get(id=todo_id).delete()
+    return redirect('/')
